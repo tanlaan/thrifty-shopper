485852efb620ff4397f7651962b53b883da54d24
Chris Bolas <tanlaan33@gmail.com>
2022-01-21 11:20:21 -0800

    Refactor products_controller & product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,83 @@ class Product < ApplicationRecord
 
   # Create new brands, categories, UPC, PLU when creating new products
   # accepts_nested_attributes_for(:brand, :category, :upc, :plu)
+
+  def self.search(query)
+    products = []
+    upc, plu, title = nil
+
+    # UPC or PLU like
+    if only_digits?(query)
+      products, upc, plu = code_search(query) unless query.length > 12
+
+    # Title search
+    else
+      products = Product.where('lower(title) like ?', "%#{query.downcase}%")
+      title = query
+    end
+
+    [products, upc, plu, title]
+  end
+
+  def self.find_by_plu(plu)
+    Product.find_by_sql('SELECT products.*
+                     FROM products
+                     JOIN plus ON products.plu_id = plus.id
+                     WHERE code = ?
+                     LIMIT 1', [plu])
+  end
+
+  def self.find_by_upc(upc)
+    Product.find_by_sql('SELECT products.*
+                     FROM products
+                     JOIN upcs ON products.upc_id = upcs.id
+                     WHERE code = ?
+                     LIMIT 1', [upc])
+  end
+
+  def self.find_like_plu(fragment)
+    Product.find_by_sql('SELECT products.*
+                     FROM products
+                     JOIN plus ON products.plu_id = plus.id
+                     WHERE code LIKE ?
+                     LIMIT 1', ["%#{fragment}%"])
+  end
+
+  def self.find_like_upc(fragment)
+    Product.find_by_sql('SELECT products.*
+                     FROM products
+                     JOIN upcs ON products.upc_id = upcs.id
+                     WHERE code LIKE ?
+                     LIMIT 1', ["%#{fragment}%"])
+  end
+
+  def self.find_by_code_fragment(fragment)
+    case fragment.length
+    when 6..11
+      products = find_like_upc(fragment)
+    when 1..3
+      products = find_like_plu(fragment)
+    end
+    products
+  end
+
+  # Check string for only digits
+  def self.only_digits?(query)
+    query.scan(/\D/).empty?
+  end
+
+  # Search for code like queries
+  def self.code_search(query)
+    case query.length
+    when 12 # UPC length
+      products = find_by_upc(query)
+      upc = query
+    when 4, 5 # PLU length
+      products = find_by_plu(query)
+      plu = query
+    else # is fragment
+      products = find_by_code_fragment(query)
+    end
+    [products, upc, plu]
+  end
 end
