742596050f2bd7a1cd36c1d40231606bc8e7620e
Chris Bolas <tanlaan33@gmail.com>
2022-01-21 08:10:35 -0800

    Edit products_controller, add labelling

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,15 @@
 class Product < ApplicationRecord
+  # Ownership
   has_many :prices
+
+  # References
   belongs_to :weight_unit, optional: true
   belongs_to :volume_unit, optional: true
   belongs_to :brand
   belongs_to :category
   belongs_to :upc, optional: true
   belongs_to :plu, optional: true
+
+  # Create new brands, categories, UPC, PLU when creating new products
+  # accepts_nested_attributes_for(:brand, :category, :upc, :plu)
 end
