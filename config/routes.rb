160e1a3eb6d6d483482692c553e475a0fd639645
Chris Bolas <tanlaan33@gmail.com>
2022-01-21 08:09:37 -0800

    Edit routes, add home and search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
   resources :prices
   resources :products
+
+  post '/search', to: 'products#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
