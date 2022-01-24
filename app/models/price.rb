b6e7b92675ed91efa695dd1a3781a1df9907b91a
Chris Bolas <tanlaan33@gmail.com>
2022-01-24 13:32:21 -0800

    Fix Price attributes

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,4 +1,16 @@
 class Price < ApplicationRecord
   belongs_to :product
   belongs_to :store
+
+  accepts_nested_attributes_for :store
+
+  def store_attributes=(store_attributes)
+    return if store_attributes[:name].blank?
+
+    self.store = Store.find_or_create_by(name: store_attributes[:name])
+  end
+
+  def by_product(product_id)
+    Price.where('product_id = ?', product_id)
+  end
 end
